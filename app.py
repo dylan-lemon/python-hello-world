7870ecd34d184ae0577308c6c96ef466862db807
Dylan Lemon <dlemon@marketstar.com>
2022-01-27 10:33:51 -0700

    testing auto deploy

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask import request
+from flask import url_for
+from markupsafe import escape
+
 app = Flask(__name__)
 
 
@@ -14,7 +18,7 @@ def who():
 
 @app.route("/hi/<username>")
 def greet(username):
-    return f"Hi there, {username}!"
+    return f"Hi there, {escape(username)}!"
 
 
 @app.route("/autoDeployTest/")
@@ -25,3 +29,26 @@ def deployed():
 @app.route("/caleb/")
 def whatever():
     return "Hi Caleb"
+
+
+@app.route('/get', methods=['GET', 'POST'])
+def get():
+    if request.method == 'POST':
+        return do_the_post()
+    else:
+        return do_the_get()
+
+
+def do_the_post():
+    return 'This is a post test'
+
+
+def do_the_get():
+    return 'This is a get test'
+
+
+with app.test_request_context():  # could be used in a index page for route options maybe?
+    print(url_for('index'))
+    print(url_for('who'))
+    print(url_for('greet', username='John Doe'))
+    print(url_for('whatever'))
